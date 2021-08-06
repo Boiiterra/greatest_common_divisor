7ce8ee4218f009cc9cfd2351186d92c8557f6b37
TerraBoii <kostyaborisenko05@gmail.com>
2021-08-06 10:38:16 +0300

    New documentation, changed position of if statement for changing answer in _gcd function

--- support.py
+++ support.py
@@ -1,36 +1,36 @@
 """Module created by TerraBoii where you can find some useful functions."""
 
 
-def rlc(amount=2, limit=100):
-    """Creates list with random numbers (from 1 to limit that is given by user(default is 100) in it.
-    Amount of elements is given by the user, default is 2.
-    rlc -> random list creator"""
-    lst = []
-    from random import randint as rng
-    for counter in range(0, amount):
+def int_list_creator(amount=2, limit=100):
+    """Creates list with random numbers (from 1 to limit that is given by user, default is 100.
+    Amount of elements is given by the user, default is 2."""
+    lst = []  # List that is going to store generated numbers
+    from random import randint as rng  # importing randint for integer generation
+    for counter in range(0, amount):  # populating list using for loop
         number = rng(1, limit)
         lst.append(number)
     return lst
 
 
 def _gcd(_list: list[int]):
-    """This function can find Greatest Common Divisor of multiple numbers"""
-    answer = 1  # Is our answer, if it isn`t changed then it will be 1
-    divisors = []  # Is a container for our divisors will be expended
-    smallest = min(_list)
-    for runner in range(1, int(smallest ** (1 / 2)) + 1):
+    """This function can find Greatest Common Divisor of multiple numbers, as argument takes list with integers."""
+    answer = 0  # This variable is going to store gcd
+    divisors = []  # Is a container for our divisors, will be expended
+    smallest = min(_list)  # To make this function faster we will find every divisor the smallest has
+    for runner in range(1, int(smallest ** (1 / 2)) + 1):  # Using square method to find divisors
         if smallest % runner == 0:
             divisors.append(runner)
     support = []  # This list is used to store another divisors
-    for runner in divisors:
+    for runner in divisors:  # Here we use first list of divisors to finish full one
         support.append(int(smallest / runner))
-    divisors.extend(support)
-    divisors = sorted(list(set(divisors)))
-    for divisor in divisors:
-        counter = 0
+    divisors.extend(support)  # Creating full list where all divisors are stored
+    divisors = sorted(list(set(divisors)))  # Deleting clones, we don`t need them
+    for divisor in divisors:  # in this block we are looking for greatest common divisor
+        counter = 0  # This variable used to count how many element could be divided by divisor
         for element in _list:
             if element % divisor == 0:
                 counter += 1
-            if counter == len(_list) and divisor > answer:
+        else:  # When we finished going through list we check if we need to change the greatest common divisor
+            if counter == len(_list) and divisor > answer:  #
                 answer = divisor
     return answer
