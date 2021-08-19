6804384cc02b681f6dd339e4a22610bbb6ace7ed
TerraBoii <kostyaborisenko05@gmail.com>
2021-08-19 13:46:04 +0300

    Changed documentation

--- support.py
+++ support.py
@@ -10,20 +10,20 @@ def int_list_creator(amount=2, limit=100):
 
 def _gcd(_list: list[int]):
     """This function can find Greatest Common Divisor of multiple numbers, as argument takes list with integers."""
-    answer = 0  # This variable is going to store gcd
-    divisors = []  # Is a container for our divisors, will be expended
-    smallest = min(_list)  # To make this function faster we will find every divisor the smallest has
+    answer = 0
+    divisors = []
+    smallest = min(_list)  # To make this function faster we will find every divisor of the smallest number in the list
     for number in range(1, int(smallest ** (1 / 2)) + 1):  # Using square method to find divisors
         if smallest % number == 0:
             divisors.append(number)
-    support = []  # This list is used to store another divisors
+    support = []
     for divisor in divisors:  # Here we use first list of divisors to finish full one
         support.append(int(smallest / divisor))
     divisors.extend(support)  # Finishing list with adding last divisors
     divisors = sorted(list(set(divisors)), reverse=True)  # Deleting clones and making list reversed
-    _list.pop(_list.index(smallest))  # Deleting smallest element because we know all it's divisors
+    _list.pop(_list.index(smallest))  # Deleting smallest element because we know it's divisors
     for divisor in divisors:  # in this block we are looking for greatest common divisor
-        counter = 0  # This variable used to count how many element could be divided by divisor
+        counter = 0
         for element in _list:
             if element % divisor == 0:
                 counter += 1
