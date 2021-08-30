814934ac82dfc051aa069403f15e43787d6a726f
TerraBoii <kostyaborisenko05@gmail.com>
2021-08-30 10:26:37 +0300

    created every_divisor function, changed _gcd returning structure + changed checking process(added break statement)

--- support.py
+++ support.py
@@ -1,27 +1,30 @@
 """Module created by TerraBoii where you can find some useful functions."""
 
 
-def _gcd(_list: list[int]):
-    """This function can find Greatest Common Divisor of multiple numbers, as argument takes list with integers."""
-    answer = 0
+def every_divisor(integer: int):
+    """Finds all divisors given number has."""
     divisors = []
-    smallest = min(_list)  # To make this function faster we will find every divisor of the smallest number in the list
-    for number in range(1, int(smallest ** (1 / 2)) + 1):  # Using square method to find divisors
-        if smallest % number == 0:
+    for number in range(1, int(integer ** (1 / 2)) + 1):  # Using square method to find divisors
+        if integer % number == 0:
             divisors.append(number)
     support = []
     for divisor in divisors:  # Here we use first list of divisors to finish full one
-        support.append(int(smallest / divisor))
+        support.append(int(integer / divisor))
     divisors.extend(support)  # Finishing list with adding last divisors
-    divisors = sorted(list(set(divisors)), reverse=True)  # Deleting clones and making list reversed
-    _list.pop(_list.index(smallest))  # Deleting smallest element because we know all it's divisors
+    return sorted(list(set(divisors)), reverse=True)  # Deleting clones and making list reversed
+
+
+def _gcd(_list: list[int]):
+    """This function can find Greatest Common Divisor of multiple numbers, as argument takes list with integers."""
+    divisors = every_divisor(min(_list))  # We will find every divisor of the smallest number in the list
+    _list.pop(_list.index(min(_list)))  # Deleting smallest element because we know all it's divisors
     for divisor in divisors:  # in this block we are looking for greatest common divisor
         counter = 0
         for element in _list:
             if element % divisor == 0:
                 counter += 1
+            else:
+                break
         else:  # When we finished going through list given by user we check if we need to change the gcd
             if counter == len(_list):  # Checking if we need to change the answer and ending loop
-                answer = divisor
-                break
-    return answer
+                return divisor
