c98225302e19b3db24217b0dc0815929c241f5a9
TerraBoii <kostyaborisenko05@gmail.com>
2021-08-30 09:36:36 +0300

    Deleted results page, message text changed in amount_error_message, MainPage changes: added new commends for buttons, label replaced by button, removed file creation process, new logic

--- app_module.py
+++ app_module.py
@@ -1,7 +1,6 @@
 from tkinter import *
 from tkinter.messagebox import showerror
 from support import _gcd
-from os import remove
 
 
 class AmountError(Exception):
@@ -18,19 +17,9 @@ def integer_value_error_message(_reason, index=None):
     showerror(title="Error", message=message)
 
 
-def amount_error_message(amount, _type: str):
-    word = example = args = ""
-    if _type == "full":
-        word = "two"
-        example = "120, 2"
-        args = "First argument is max possible integer,\nsecond - amount" \
-               f"\nBut you entered only {amount}"
-    elif _type == "custom":
-        word = "multiple"
-        example = "12, 6, 24"
-        args = f"\nBut you entered only {amount}"
-    message = f"Incorrect input, you are supposed to enter {word} integers " \
-              f"\n(e.g. {example}). {args}."
+def amount_error_message(amount):
+    message = "Incorrect input, you are supposed to enter multiple integers " \
+              f"\n(e.g. 12, 6, 24). \nBut you entered only {amount}."
     showerror(title="Error", message=message)
 
 
@@ -50,7 +39,7 @@ class MainAppBody(Tk):
 
         self.frames = {}
 
-        frame_collection = (StartPage, MainPage, ResultsPage)
+        frame_collection = (StartPage, MainPage)
 
         for frame in frame_collection:
             current_frame = frame(container, self)
@@ -74,7 +63,7 @@ class StartPage(Frame):
         text = "This program takes user's\n" \
                "integers and finds their greatest common divisor"
 
-        label = Label(self, text=text, font=("Times New Roman", 17), bg="black", fg="#00ff00")
+        label = Label(self, text=text, font=("Times New Roman", 18), bg="black", fg="#00ff00")
         label.pack(padx=5, pady=11, fill=BOTH)
 
         main_page_button = Button(self, text="\nStart\n", bg="#0a0c0a", fg="#00ff00", font=("Arial", 30),
@@ -89,22 +78,19 @@ class MainPage(Frame):
     def __init__(self, parent, controller):
         Frame.__init__(self, parent, bg="Black")
 
-        text = "Here you can enter your integers (e.g. 120, 20)"
+        def reset():
+            text_area.config(state=NORMAL)
+            text_area.delete("0.0", END)
+            confirm_button.config(state=ACTIVE, bg="#0a0a0c", text="Confirm")
+            info_plus_reset.config(text=text, state=DISABLED, bg="black")
 
-        label = Label(self, text=text, font=("Times New Roman", 18), bg="black", fg="#00ff00")
-        label.pack(padx=5, fill=BOTH)
+        def return_to_home():
+            text_area.config(state=NORMAL)
+            text_area.delete("0.0", END)
+            confirm_button.config(state=ACTIVE, bg="#0a0a0c", text="Confirm")
+            controller.show_frame(StartPage)
 
-        text_area = Text(self, font=("Times New Roman", 13), bg="black", fg="#00ff00",
-                         selectbackground="white", selectforeground="#ff00ff",
-                         insertbackground="white", width=55, height=5, wrap=WORD)
-        text_area.pack(padx=5, pady=2)
-
-        home_button = Button(self, text="Return home", bg="#0a0a0a", fg="#00ff00", font=("Arial", 22),
-                             activeforeground="green", activebackground="black", bd=0,
-                             command=lambda: controller.show_frame(StartPage))
-        home_button.pack(fill=BOTH, side=BOTTOM)
-
-        def check_if_valid():
+        def check_and_show():
             _input_str = text_area.get('0.0', END)
             try:
                 integers = list(map(int, _input_str.split(', ')))
@@ -113,70 +99,48 @@ class MainPage(Frame):
                 if len(integers) < 2:
                     raise AmountError(f"{len(integers)}")
                 text_area.delete('0.0', END)
-                file = open("temporary_storage.txt", 'w')
-                file.seek(0)
-                file.write(str(integers)[1:-1])
-                file.close()
+                show_results(_input_str)
                 return True
             except ValueError as reason:
                 text_area.delete('0.0', END)
                 if str(reason)[41:42] == "0":
                     integer_value_error_message(str(reason)[41:42], int(str(reason)[45:-1]))
                 else:
-                    integer_value_error_message(str(reason)[41:42])
+                    integer_value_error_message(str(reason)[41:-1])
             except AmountError as error_message_fragment:
+                print(error_message_fragment)
                 text_area.delete("1.0", END)
-                amount_error_message(error_message_fragment, "custom")
+                amount_error_message(error_message_fragment)
 
-        confirm_button = Button(self, text="Confirm", bg="#0a0a0c", fg="#00ff00", font=("Arial", 22),
-                                activeforeground="green", activebackground="black", bd=0,
-                                command=lambda: controller.show_frame(ResultsPage)
-                                if check_if_valid() else None)
-        confirm_button.pack(fill=BOTH)
-
-
-class ResultsPage(Frame):
-
-    def __init__(self, parent, controller):
-        Frame.__init__(self, parent, bg="Black")
-
-        def getting_results():
-            file = open('temporary_storage.txt', 'r')
-            string_results = file.read()
-            results = list(map(int, string_results.split(', ')))
-            file.close()
-            remove("temporary_storage.txt")
-            text_area.delete("1.0", END)
-            text_area.insert('0.0', f"Entered {len(results)} numbers:\n")
-            text_area.insert(END, string_results)
+        def show_results(string_numbers: str):
+            integers = list(map(int, string_numbers.split(', ')))
+            text_area.insert('0.0', f"Entered {len(integers)} numbers:\n")
+            text_area.insert(END, string_numbers)
             text_area.insert(END, "\nGreatest common divisor: ")
-            text_area.insert(END, str(_gcd(results)) + '\n')
+            text_area.insert(END, _gcd(integers))
             text_area.config(state=DISABLED)
-            home_button.config(state=ACTIVE)
-            launch_button.config(state=DISABLED)
+            confirm_button.config(state=DISABLED, bg="Black", text="Now you can return home or reset field")
+            info_plus_reset.config(text="Reset", state=ACTIVE, bg="#0c0a0a")
 
-        def return_to_home():
-            text_area.config(state=NORMAL)
-            text_area.delete("1.0", END)
-            text_area.insert('0.0', text)
-            home_button.config(state=DISABLED)
-            launch_button.config(state=ACTIVE)
-            controller.show_frame(StartPage)
+        text = "Here you can enter your integers (e.g. 120, 20)"
 
-        text = 'Press "Finish" button to get results, or type here for fun'
+        info_plus_reset = Button(self, text=text, font=("Times New Roman", 17), bg="black", fg="#00ff00",
+                                 activeforeground="green", activebackground="black",
+                                 disabledforeground="#00ff00", bd=0, state=DISABLED,
+                                 command=reset)
+        info_plus_reset.pack(fill=BOTH)
 
         text_area = Text(self, font=("Times New Roman", 13), bg="black", fg="#00ff00",
                          selectbackground="white", selectforeground="#ff00ff",
-                         insertbackground="white", width=55, height=6, wrap=WORD)
-        text_area.insert("0.0", text)
-        text_area.pack(padx=5, pady=10)
+                         insertbackground="white", width=55, height=5, wrap=WORD)
+        text_area.pack(padx=5, pady=2)
 
-        home_button = Button(self, text="Return home", bg="#0a0a0a", fg="#00ff00", font=("Arial", 24),
-                             activeforeground="green", activebackground="black", bd=0, state=DISABLED,
-                             command=return_to_home, disabledforeground="black")
+        home_button = Button(self, text="Return home", bg="#0a0a0a", fg="#00ff00", font=("Arial", 21),
+                             activeforeground="green", activebackground="black", bd=0,
+                             command=return_to_home)
         home_button.pack(fill=BOTH, side=BOTTOM)
 
-        launch_button = Button(self, text="Finish", bg="#0a0a0c", fg="#00ff00", font=("Arial", 24),
-                               activeforeground="green", activebackground="black", bd=0,
-                               command=getting_results, disabledforeground="black")
-        launch_button.pack(fill=BOTH, side=BOTTOM, pady=1)
+        confirm_button = Button(self, text="Confirm", bg="#0a0a0c", fg="#00ff00", font=("Arial", 21),
+                                activeforeground="green", activebackground="black", bd=0,
+                                command=check_and_show, disabledforeground="#00ff00")
+        confirm_button.pack(fill=BOTH)
